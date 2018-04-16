326c9334d0efa77aa91ecfc4a1bc42b62e730692
Joanna <jmgsese@gmail.com>
2018-04-16 12:48:03 -0500

    Test hello world

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+class App extends Component { //component is named 'App'
+  render() { // element returned is specified in render method
+    const helloWorld = 'Welcome to the Road to learn React with a variable';
+    const starfish = 'This is a starfish';
+    const user = {firstName: 'Jelly', lastName: 'Belly'};
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>{helloWorld}</h2>
+        <p>{starfish}.</p>
+        <p>His first name is {user.firstName}.</p>
+        <p>His last name is {user['lastName']}.</p>
       </div>
     );
   }
